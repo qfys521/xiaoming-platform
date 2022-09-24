c1e65dca6ed3fc95ff9d1f7ec5e6fcd904151c5f
Chuanwise <chuanwise@qq.com>
2022-09-24 22:21:06 +0800

    add Terminal interface

---
+++ xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/terminal/Terminal.java
@@ -0,0 +1,12 @@
+package cn.codethink.xiaoming.platform.terminal;
+
+/**
+ * <h1>终端</h1>
+ *
+ * <p>终端是用于输入输出文字的工具，通常用于输入控制台命令和输出日志信息。
+ * 小明平台不会自动注册任何终端，所有的终端都是第三方提供的。</p>
+ *
+ * @author Chuanwise
+ */
+public interface Terminal {
+}
