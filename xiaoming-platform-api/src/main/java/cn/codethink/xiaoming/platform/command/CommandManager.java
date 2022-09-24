6334e81d6347a7b09dbd0fde92ae1cb067017c9f
Chuanwise <chuanwise@qq.com>
2022-09-24 22:59:29 +0800

    add CommandManager

---
+++ xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/command/CommandManager.java
@@ -0,0 +1,13 @@
+package cn.codethink.xiaoming.platform.command;
+
+/**
+ * <h1>指令管理器</h1>
+ *
+ * <p>管理指令相关的组件，如参数解析器、自动装配器，以及指令的注册和注销。</p>
+ *
+ * @author Chuanwise
+ */
+public interface CommandManager {
+
+
+}
