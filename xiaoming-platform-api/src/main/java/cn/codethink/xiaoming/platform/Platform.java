3f920806160da1481be430f2e45fd0f1f18bab40
Chuanwise <chuanwise@qq.com>
2022-09-25 01:10:10 +0800

    add modules getter to Platform

--- xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/Platform.java
+++ xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/Platform.java
@@ -1,5 +1,11 @@
 package cn.codethink.xiaoming.platform;
 
+import cn.codethink.xiaoming.platform.command.CommandManager;
+import cn.codethink.xiaoming.platform.event.EventManager;
+import cn.codethink.xiaoming.platform.plugin.PluginManager;
+import cn.codethink.xiaoming.platform.service.ServiceManager;
+import cn.codethink.xiaoming.platform.terminal.TerminalManager;
+
 /**
  * <h1>小明平台</h1>
  *
@@ -8,4 +14,39 @@ package cn.codethink.xiaoming.platform;
  * @author Chuanwise
  */
 public interface Platform {
+    
+    /**
+     * 获取事件管理器
+     *
+     * @return 事件管理器
+     */
+    EventManager getEventManager();
+    
+    /**
+     * 获取服务管理器
+     *
+     * @return 服务管理器
+     */
+    ServiceManager getServiceManager();
+    
+    /**
+     * 获取终端管理器
+     *
+     * @return 终端管理器
+     */
+    TerminalManager getTerminalManager();
+    
+    /**
+     * 获取插件管理器
+     *
+     * @return 插件管理器
+     */
+    PluginManager getPluginManager();
+    
+    /**
+     * 获取指令管理器
+     *
+     * @return 指令管理器
+     */
+    CommandManager getCommandManager();
 }
