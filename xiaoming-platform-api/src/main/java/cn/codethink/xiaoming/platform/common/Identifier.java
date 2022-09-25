f183b8532c2a282b0b15c67397c0307ee42c8d1a
Chuanwise <chuanwise@qq.com>
2022-09-25 23:50:49 +0800

    add Identifier

---
+++ xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/common/Identifier.java
@@ -0,0 +1,20 @@
+package cn.codethink.xiaoming.platform.common;
+
+/**
+ * <h1>标识符</h1>
+ *
+ * <p>带有命名空间和名字的字符串</p>
+ *
+ * @author Chuanwise
+ */
+public interface Identifier {
+    
+    /**
+     * 获取名字
+     *
+     * @return 名字
+     */
+    String getName();
+    
+    
+}
