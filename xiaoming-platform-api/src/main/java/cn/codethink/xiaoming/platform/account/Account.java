282a66ae3305bb7a4ef1f57a628cb5e4425fafae
Chuanwise <chuanwise@qq.com>
2022-09-25 08:21:36 +0800

    add account interface

---
+++ xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/account/Account.java
@@ -0,0 +1,13 @@
+package cn.codethink.xiaoming.platform.account;
+
+public interface Account {
+    
+    // Code getCode()
+    
+    /**
+     * 获取账户显示名
+     *
+     * @return 账户显示名
+     */
+    String getDisplayName();
+}
