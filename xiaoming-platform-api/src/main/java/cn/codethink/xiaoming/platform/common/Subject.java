df1241fc8ef69fd8a661bfb6a1ca02b312fb20e1
Chuanwise <chuanwise@qq.com>
2022-09-25 23:48:32 +0800

    add methods to Subject

--- xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/common/Subject.java
+++ xiaoming-platform-api/src/main/java/cn/codethink/xiaoming/platform/common/Subject.java
@@ -1,11 +1,27 @@
 package cn.codethink.xiaoming.platform.common;
 
 /**
- * <h1>注册主体</h1>
+ * <h1>主体</h1>
  *
- * <p>可以注册某种组件（监听器、指令、服务、权限等）的对象。</p>
+ * <p>可以注册某种组件（监听器、指令、服务、权限等）的对象、进行某些操作（广播事件等）的对象。</p>
  *
  * @author Chuanwise
  */
 public interface Subject {
+    
+    /**
+     * 获取主体名
+     *
+     * @return 主体名
+     */
+    String getName();
+    
+    /**
+     * 获取主体显示名
+     *
+     * @return 主体显示名
+     */
+    String getDisplayName();
+    
+    
 }
